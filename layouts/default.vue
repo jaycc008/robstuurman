d8f85f06bb2727c21384f33846471082b4cb1aed
Jayce Ardon <jayce.ardon@gmail.com>
2021-02-17 15:54:19 +0100

    added content below carousel for desktop

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,6 +3,37 @@ v-app(light)
 	v-main
 		main-nav
 		nuxt.content
+		v-footer(padless)
+			v-card.lightpurple(flat tile width="100%")
+				v-container
+					v-row.my-5
+						v-col.offset-md-1(cols="12" md="5")
+							.full-height.d-flex.flex-column.justify-center
+								h1 Contact
+								.title2.mb-10 Zorg goed voor je zelf, neem gerust contact met me op
+								ul
+									li.d-inline-flex.address-list-item
+										v-img.mr-4(:src="require('~/assets/mail.svg')" height="40" max-width="40")
+										p.address-line rob.stuurman@hotmail.com
+									li.d-inline-flex.address-list-item
+										v-img.mr-4(:src="require('~/assets/phone.svg')" height="40" max-width="40")
+										p.address-line 06 13 15 69 55
+									li.d-inline-flex.address-list-item
+										v-img.mr-4(:src="require('~/assets/home.svg')" height="40" max-width="40")
+										p.address-line Lupinestraat 15, 5492KD, Sint-Oedenrode
+						v-col.offset-md-1(cols="4" v-if="$vuetify.breakpoint.mdAndUp")
+							.full-height.d-flex.justify-center
+								v-img(:src="require('~/assets/argoem.png')" contain)
+			.full-width.d-flex.justify-end.my-3
+				v-menu(offset-y top)
+					template(v-slot:activator="{ on, attrs }")
+						a.small.mr-6.secondary--text.text-decoration-underline(v-bind="attrs" v-on="on") Credits
+					v-list
+						v-list-item(v-for="(credit, index) in credits" :key="index")
+							v-list-item-title.small
+								| {{credit.text}}
+								a.secondary--text.text-decoration-underline(v-bind:href="credit.href" _target="_blank")  {{credit.linktext}}
+				.mr-6.small 2020 © Rob Stuurman
 </template>
 
 <script>
@@ -10,6 +41,27 @@ import MainNav from '@/components/main-nav'
 
 export default {
 	components: { MainNav },
+	data() {
+		return {
+			credits: [
+				{
+					text: 'Ontworpen door',
+					linktext: 'Moor Design',
+					href: 'www.souliamoor.nl',
+				},
+				{
+					text: 'Ontwikkeld door',
+					linktext: 'Jayce Ardon',
+					href: 'www.ja-development.nl',
+				},
+				{
+					text: 'Illustraties door',
+					linktext: 'Freepik',
+					href: 'www.freepik.com',
+				},
+			],
+		}
+	},
 }
 </script>
 
@@ -24,4 +76,19 @@ export default {
 	@media #{map-get($display-breakpoints, 'sm-and-down')}
 		.content
 			margin-top: 128px
+
+.full-height
+	height: 100%
+
+.full-width
+	width: 100%
+
+.address-list-item
+	width: 100%
+
+.address-line
+	line-height: 40px
+
+.v-menu__content
+	box-shadow: 0px 0px 4px 4px rgba(0, 0, 0, 0.04) !important
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -15,7 +15,7 @@ v-container
 			img(src="~/assets/zen.png")
 		v-col
 			portal-target(name="destination")
-	v-row
+	v-row.float-left
 		v-col.offset-md-1.d-flex(cols="12" md="5" order-md="2")
 			.about-me
 				h1 Over Rob Stuurman
@@ -26,7 +26,86 @@ v-container
 		v-col.offset-1.offset-md-1(cols="10" md="4" order-md="1")
 			.about-picture
 				v-img.image-shadow(:src="require('~/assets/rob.png')")
-
+	v-row.float-left
+		v-col.offset-md-1.mb-16(md="8")
+			h1 Energetische behandelingen
+			.title2.mb-6 Als energetisch genezer kan ik u helpen bij psychische, emotionele en lichamelijke klachten, daar waar de reguliere medische zorg geen hulp meer kan bieden
+			p Alternatieve genezing wil niet de plaats innemen van de reguliere medische zorg. Het is van belang om daarvan, waar mogelijk gebruik te blijven maken.
+		v-col(cols="12")
+			.placeholder.d-flex.justify-center.align-center.mb-13 placeholder
+	v-row.float-left.mb-16
+		v-col.py-0.py-sm-5.offset-md-1(md="6" cols="12")
+			p.mb-8 Voor het energetisch behandelen stel ik mij in verbinding met de krachten van
+				span.focus  zowel de aarde als de geestelijke wereld
+				| . Het kosmische symbool ’Argoem’, dat afgebeeld is op de vloer, versterkt hierbij mijn verbinding en de kracht tot genezing.
+		v-col.py-0.py-sm-5.offset-md-1(md="5" cols="12" sm="6")
+			p.small In de loop van de jaren heb ik een ontwikkeling in innerlijke groei doorgemaakt en heb ik mij daarbij opengesteld voor het
+				span.small.focus  Christusbewustzijn
+				| . Persoonlijk put ik dan ook mijn kracht uit de verbinding met God de Vader, Christus, Moeder Maria en de Engelen. Ik voel deze universele energie door mij heen stromen en gebruik deze via mijn handen om te genezen. 
+			p.small Onderdeel van de behandeling is het in balans brengen van de
+				span.small.focus  chakra's
+				| . Daarna laat ik mij intuïtief leiden door de gegeven energie. Op deze manier bereik ik goede resultaten bij geestelijke, psychische en traumatische klachten maar ook ter bestrijding van lichamelijke pijn in het algemeen zoals migraine en hoofdpijn.
+		v-col.py-0.py-sm-5(md="5" cols="12" sm="6")
+			p.small Ook maak ik gebruik van de technieken van
+				span.small.focus  Quantum Touch
+				| . Hiermee kunnen er snelle en opzienbare resultaten worden bereikt bij lichamelijke klachten en pijnen. Dit in het bijzonder bij disbalans van het beenderstelsel en bijbehorende spieren, zoals scheefstand van heupen, de wervels van de rug, nek of schedel.
+			p.small Lichamelijke aandoeningen hebben echter vaak een geestelijke oorzaak. Dit komt uiteraard ook aan de orde tijdens de behandeling.
+	v-row.mb-16.float-left
+		v-col(lg="3" sm="6")
+			.custom-card.big-shadow.d-flex.flex-sm-column.pa-8.rounded-lg
+				v-img.image-holder(:src="require('~/assets/step-1.svg')" max-height="251" contain)
+				.step-text.ml-6.ml-sm-0.mt-sm-6
+					p.text-sm-center
+						| Eerst maken we telefonisch een
+						span.focus  afspraak
+		v-col(lg="3" sm="6")
+			.custom-card.big-shadow.d-flex.flex-sm-column.pa-8.rounded-lg
+				v-img.image-holder(:src="require('~/assets/step-2.svg')" max-height="251" contain)
+				.step-text.ml-6.ml-sm-0.mt-sm-6
+					p.text-sm-center
+						| Dan volgt er een
+						span.focus  intakegesprek
+		v-col(lg="3" sm="6")
+			.custom-card.big-shadow.d-flex.flex-sm-column.pa-8.rounded-lg
+				v-img.image-holder(:src="require('~/assets/step-3.svg')" max-height="251" contain)
+				.step-text.ml-6.ml-sm-0.mt-sm-6
+					p.text-sm-center
+						| Vervolgens gaan we
+						span.focus  jouw levensloop
+						|  en wensen na
+		v-col(lg="3" sm="6")
+			.custom-card.big-shadow.d-flex.flex-sm-column.pa-8.rounded-lg
+				v-img.image-holder(:src="require('~/assets/step-4.svg')" max-height="251" contain)
+				.step-text.ml-6.ml-sm-0.mt-sm-6
+					p.text-sm-center
+						| De duur van een
+						span.focus  behandeling
+						|  kan variëren van 1-2 uur
+	v-row.mb-6
+		v-col.offset-md-1(cols="12" md="8")
+			h1 Thema avonden
+			.title2 Naast energetisch genezen bieden we ook verschillende thema avonden met andere behandelaars aan.
+	v-row.mb-6
+		v-col.offset-md-2(cols="12" md="4")
+			v-card.rounded-lg(color="skin" flat)
+				v-img(:src="require('~/assets/theme-1.png')")
+				.pa-8
+					p.small.focus Behandelaar: Rob
+					h2.tertiary--text.mb-2 Infrarood massagetafel behandeling
+					p.small.tertiary--text.line-clamp De warmte die van de infrarood afkomt, zorgt voor een betere doorbloeding en afname van pijn. In combinatie met een behandeling zorgt dit voor een betere psychische, emotionele en lichamelijke gesteldheid.
+					v-btn.gradient.text-none(color="tertiary" height="48" outlined rounded nuxt x-large)
+						span.small Aanmelden
+						img.ml-6(src="~/assets/arrow-right.svg")
+		v-col(cols="12" md="4")
+			v-card.rounded-lg(color="skin" flat)
+				v-img(:src="require('~/assets/theme-2.png')")
+				.pa-8
+					p.small.focus Reiki Master: Lya
+					h2.tertiary--text.mb-2 Reiki healingavond door Reikimaster Lya
+					p.small.tertiary--text.line-clamp Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.
+					v-btn.gradient.text-none(color="tertiary" height="48" outlined rounded nuxt)
+						span.small Aanmelden
+						img.ml-3(src="~/assets/arrow-right.svg")
 </template>
 
 <style lang="sass">
@@ -56,4 +135,32 @@ v-container
 	flex-direction: column
 	justify-content: space-around
 	margin: auto
+
+.placeholder // temporary class to fill up space
+	background-color: black
+	color: white
+	width: 100%
+	height: 470px
+
+.custom-card
+	height: 100%
+	.image-holder
+		flex: 0 1 auto
+		@media #{map-get($display-breakpoints, 'xs-only')}
+			flex: 0 0 135px
+	.step-text
+		display: flex
+		flex: 1 1 auto
+		@media #{map-get($display-breakpoints, 'xs-only')}
+			flex: 0 1 auto
+		p
+			margin: auto
+
+.line-clamp
+	text-overflow: ellipsis
+	overflow: hidden
+	display: -webkit-box !important
+	-webkit-line-clamp: 2
+	-webkit-box-orient: vertical
+	white-space: normal
 </style>
