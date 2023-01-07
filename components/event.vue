78676d82b9eb8b6535b6144fe872f247cf221614
Jayce Ardon <jayce.ardon@gmail.com>
2023-01-07 11:20:51 -0600

    new dates

--- components/event.vue
+++ components/event.vue
@@ -1,9 +1,9 @@
 <template lang="pug">
 .event-wrapper.px-10.pt-8.pb-10
-	p.small.focus.secondary--text 6 november 2022 van 11.00 - 17.00
-	h2.mt-4 Paranormale beurs Likrali
-	p.mt-2 Witte hoeve wasstraat 5
-	v-btn.gradient.mt-2.text-none.hover--white(color="tertiary" target="_blank" outlined rounded nuxt x-large href="http://likrali.nl/agenda/event/beurs-likrali/")
+	p.small.focus.secondary--text 22 januari 2023 11.00 - 17.00
+	h2.mt-4 Paranormale/Welzijnsbeurs Lightning - Zaal "Den Tref"
+	p.mt-2 Oude Provincialeweg 21 te Hapert
+	v-btn.gradient.mt-2.text-none.hover--white(color="tertiary" target="_blank" outlined rounded nuxt x-large href="https://www.facebook.com/photo/?fbid=6288118241215424&set=pcb.6288260817867833")
 		| Meer informatie
 		img.ml-6(src="~/assets/arrow-right.svg")
 </template>
