84c9bd7b330a65904160b1e6fa5a9c0c530fb5c3
Jayce Ardon <jayce.ardon@gmail.com>
2023-01-25 16:34:42 -0600

    event

--- components/event.vue
+++ components/event.vue
@@ -1,9 +1,9 @@
 <template lang="pug">
 .event-wrapper.px-10.pt-8.pb-10
-	p.small.focus.secondary--text 22 januari 2023 11.00 - 17.00
-	h2.mt-4 Paranormale/Welzijnsbeurs Lightning - Zaal "Den Tref"
-	p.mt-2 Oude Provincialeweg 21 te Hapert
-	v-btn.gradient.mt-2.text-none.hover--white(color="tertiary" target="_blank" outlined rounded nuxt x-large href="https://www.facebook.com/photo/?fbid=6288118241215424&set=pcb.6288260817867833")
+	p.small.focus.secondary--text 26 februari 2023 11.00 - 17.00
+	h2.mt-4 Zielsgelukkig Spirituele Beurs - Kloosterhotel Willibrordhaeghe
+	p.mt-2 Vlierdenseweg 109 te Deurne
+	v-btn.gradient.mt-2.text-none.hover--white(color="tertiary" target="_blank" outlined rounded nuxt x-large href="https://zielsgelukkigverbinding.nl/")
 		| Meer informatie
 		img.ml-6(src="~/assets/arrow-right.svg")
 </template>
